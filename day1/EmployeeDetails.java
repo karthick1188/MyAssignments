0384820ce05cc02c00fc2260a71b1158adbc3cd6
karthick1188 <karthickg1188@gmail.com>
2026-02-07 06:57:01 +0530

    Add files via upload
    day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		byte age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println("Name"+" "+name);
+		System.out.println("Age"+" "+age);
+		System.out.println("Gender"+" "+gender);
+		System.out.println("Accept Terms"+" "+acceptTerms);
+		System.out.println("Rating"+" "+rating);
+		System.out.println("Mobile Number"+" "+mobileNumber);
+		System.out.println("PinCode"+" "+pinCode);
+		System.out.println("Lucky Number"+" "+luckyNumber);
+		
+		
+
+	}
+
+}
